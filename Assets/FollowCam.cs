d8499a8c65c53816bc51d6f57294fc92a919381f
Azzzzzzzzzz <31974708+Azzzzzzzzzz@users.noreply.github.com>
2018-03-19 17:00:38 +1000

    Demo of the VR environment. Shows how the mirror will interact and reflect the client's avatar and goal position.

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+    GameObject mainCamera;
+    GameObject mirrorObject;
+	// Use this for initialization
+	void Start () {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+        mirrorObject = GameObject.FindWithTag("Mirror");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mirrorObject.transform.position = mainCamera.transform.position + new Vector3(0, 0, 10);
+	}
+}
